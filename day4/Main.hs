7854c16f02156b10786289b847af9378e4548e57
Oisín Mac Fhearaí <denpashogai@gmail.com>
2020-12-05 04:19:29 +0000

    Day 3 part 2

--- day4/Main.hs
+++ day4/Main.hs
@@ -1,19 +1,54 @@
 module Day3 where
 
 import Data.List (isInfixOf)
+import Data.Ix (inRange)
+import Control.Monad (guard, void)
+import Text.Parsec
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
 
 main = do
   txt <- readFile "input.txt"
   let ps = splitPassports txt
-
   print $ length $ filter isValid ps
 
-splitPassports s = rec s "" []
+passportValidator :: Parsec String () [String]
+passportValidator =
+  many1 $ do
+    k <- count 3 lower
+    char ':'
+    case k of
+      "byr" -> validateYear 1920 2002
+      "iyr" -> validateYear 2010 2020
+      "eyr" -> validateYear 2020 2030
+      "hgt" -> do
+        ds <- many1 digit
+        unit <- string "cm" <|> string "in"
+        guard $
+          inRange (if unit == "cm" then (150,193) else (59,76)) $ read ds
+      "hcl" -> do
+        char '#'
+        void $ count 6 $ oneOf "0123456789abcdef"
+      "ecl" -> void $ choice $
+        map (try . string) ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+      "pid" -> void $ count 9 digit
+      "cid" -> void $ many1 digit
+    spaces
+
+    return k
+
+validateYear :: Int -> Int -> Parsec String () ()
+validateYear a b = do
+  year <- count 4 digit
+  guard $ inRange (a, b) $ read year
+
+splitPassports s = reverse $ rec s "" []
   where rec "" ps acc = reverse ps : acc
         rec ('\n':'\n':rest) ps acc = rec rest "" (reverse ps : acc)
         rec (x:rest) ps acc = rec rest (x:ps) acc
 
-
 isValid :: String -> Bool
-isValid passport = all (`isInfixOf` passport) keys
+isValid ps = case parse passportValidator "" ps of
+               (Right fields) -> all (`elem` fields) keys
+               _ -> False
   where keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
