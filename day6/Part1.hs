3ffb6661db06a7ac4bd2b0b8c635c5be0d515990
Oisín Mac Fhearaí <denpashogai@gmail.com>
2020-12-06 05:50:55 +0000

    Day 6 part 1

---
+++ day6/Part1.hs
@@ -0,0 +1,14 @@
+module Part1 where
+
+import Data.Char (isAlpha)
+import Data.List (nub)
+
+main = do
+  txt <- readFile "input.txt"
+  let ps = splitGroups txt
+  print $ sum $ map (length. nub . filter isAlpha) ps
+
+splitGroups s = reverse $ rec s "" []
+  where rec "" ps acc = reverse ps : acc
+        rec ('\n':'\n':rest) ps acc = rec rest "" (reverse ps : acc)
+        rec (x:rest) ps acc = rec rest (x:ps) acc
