b7003fa8123d194ed11a91e7d31d0b3822e48dc6
Oisín Mac Fhearaí <denpashogai@gmail.com>
2020-12-06 05:59:04 +0000

    Day 6 part 2

--- day6/Part2.hs
+++ day6/Part2.hs
@@ -1,12 +1,12 @@
-module Part1 where
+module Part2 where
 
 import Data.Char (isAlpha)
-import Data.List (nub)
+import Data.List (intersect)
 
 main = do
   txt <- readFile "input.txt"
   let ps = splitGroups txt
-  print $ sum $ map (length . nub . filter isAlpha) ps
+  print $ sum $ map (length . foldl1 intersect . lines) ps
 
 splitGroups s = reverse $ rec s "" []
   where rec "" ps acc = reverse ps : acc
