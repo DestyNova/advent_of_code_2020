5882228e0ade5a6d828bb21d9b368f857a7b4d2f
Oisín <denpashogai@gmail.com>
2020-12-18 15:12:53 +0000

    Update Main.hs

--- day1/Main.hs
+++ day1/Main.hs
@@ -3,6 +3,4 @@ module Main where
 main = do
   txt <- readFile "input.txt"
   let nums = read <$> lines txt
-  print $ findSum nums 2020
-
-findSum nums n = head [ x*y*z | x <- nums, y <- nums, z <- nums, x+y+z == n ]
+  print $ head [ x*y*z | x <- nums, y <- nums, z <- nums, x+y+z == 2020 ]
