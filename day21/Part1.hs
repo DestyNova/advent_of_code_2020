5d74b2ddbd4c952cf05a629b19b017007b65ef5b
Oisín Mac Fhearaí <denpashogai@gmail.com>
2020-12-21 05:59:35 +0000

    Tidy

--- day21/Part1.hs
+++ day21/Part1.hs
@@ -1,34 +1,20 @@
 module Main where
 
-import Text.Parsec
-import Data.Graph
+import Data.List (intersect, nub)
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.List (intersect, nub)
+import Text.Parsec
 
 main = do
   txt <- readFile "input.txt"
-  -- let s = parse ruleParser "" txt
-  -- print s
   let (Right (ingredientCounts, allergens)) = parse ruleParser "" txt
-  putStrLn "Ingredients:"
-  print ingredientCounts
-  putStrLn "Allergens:"
-  print allergens
   let safeIngredients = getSafeIngredients ingredientCounts allergens
-  print safeIngredients
-  print $ sum (map snd $ Map.toList safeIngredients)
-  -- print $ sum [n | ingredient <- safeIngredients, (Just n) <- Map.lookup ingredientCounts]
+  print $ sum $ snd <$> Map.toList safeIngredients
 
 getSafeIngredients :: Map String Int -> Map String [String] -> Map String Int
 getSafeIngredients ingredientCounts allergens =
   let dodgyIngredients = nub $ concatMap snd (Map.toList allergens)
     in Map.filterWithKey (\k _ -> k `notElem` dodgyIngredients) ingredientCounts
-
-graphHolds bags = let (g, node, vert) = graphFromEdges $ map (\(k, ks) -> ((), k, ks)) bags
-                      (Just k) = vert "shiny gold"
-                  in
-                      length (reachable (transposeG g) k) - 1
 
 ruleParser :: Parsec String () (Map String Int, Map String [String])
 ruleParser = do
@@ -45,6 +31,4 @@ ruleParser = do
         ) (string "\n")
   let counts = Map.fromListWith (+) $ concatMap fst ps
   let allergens = Map.fromListWith intersect (concatMap snd ps)
-          -- map (\(code, tx) -> (code, [tx])) $ concatMap snd ps
   return (counts, allergens)
-
